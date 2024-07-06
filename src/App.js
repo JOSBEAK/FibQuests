c47091b76e1102ed8ce00b57c0519cbca7825fd1
ANURAG PANDEY <drecothea@ANURAGs-MacBook-Air.local>
2024-07-07 00:58:29 +0530

    Responsive design completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setDueToday,
+  setSolvedToday,
+  setCurrentDate,
+  setIsLoggedIn,
+} from "./redux/store";
+import Navbar from "./components/Navbar";
+import PreLogin from "./components/PreLogin";
+import PostLogin from "./components/Dashboard";
 
 function App() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.app.isLoggedIn);
+
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    const today = new Date().toISOString().slice(0, 10);
+    dispatch(setCurrentDate(today));
+    fetchDueAndSolvedOnDate(today);
+    checkSession();
+  }, [dispatch]);
+
+  const checkSession = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/check_session");
+      dispatch(setIsLoggedIn(response.data.logged_in));
+      console.log("Session check:", response.data);
+    } catch (error) {
+      console.error("Session check error:", error);
+    }
+  };
+
+  const fetchDueAndSolvedOnDate = async (date) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:5000/due_and_solved_on_date",
+        { date }
+      );
+      dispatch(setDueToday(response.data.due_today));
+      dispatch(setSolvedToday(response.data.solved_today));
+    } catch (error) {
+      console.error("Error fetching due and solved questions on date:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 1,
+        minHeight: "100vh",
+
+        background:
+          "linear-gradient(270deg, rgba(13, 0, 17, 1) 0%, rgba(2, 45, 65, 1) 99%)",
+      }}
+    >
+      <Navbar />
+      <Box padding={"40px 30px"}>
+        {isLoggedIn ? <PostLogin /> : <PreLogin />}
+      </Box>
+    </Box>
   );
 }
 
